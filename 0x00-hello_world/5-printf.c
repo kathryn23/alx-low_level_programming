d7c435937bcb11735dc441e589a4f8467c7fc883
kathryn23 <katecathy00@.com>
2023-08-10 18:20:49 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-
-int main() {
-    printf("In the realm of imagination,\n");
-    printf("Colors dance upon the canvas of dreams,\n");
-    printf("Brushstrokes of code paint ethereal scenes,\n");
-    printf("Where logic and beauty intertwine as it seems.\n");
-   
-    return 0;
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point of the program. It performs...
+ * (Explain what the main function does and its purpose.)
+ *
+ * Return: Return value (if any)
+ */
+int main(void)
+{
+	printf("In the realm of imagination,\n");
+	printf("Colors dance upon the canvas of dreams,\n");
+	printf("Brushstrokes of code paint ethereal scenes,\n");
+	printf("Where logic and beauty intertwine as it seems.\n");
+	return (0);
 }
