def5562394cb4dd6a2ab0f5a0ac95c0601b5a8b0
kathryn23 <katecathy00@.com>
2023-08-10 17:36:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point of the program. It performs...
+ * (Explain what the main function does and its purpose.)
+ *
+ * Return: Return value (if any)
+ */
 int main(void)
 {
 	puts(""Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
