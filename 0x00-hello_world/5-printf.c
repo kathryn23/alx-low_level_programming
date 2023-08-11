11e73becefd71424876340d65a7ca876ef785e88
kathryn23 <katecathy00@.com>
2023-08-10 17:57:40 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("In the realm of imagination,\n");
+    printf("Colors dance upon the canvas of dreams,\n");
+    printf("Brushstrokes of code paint ethereal scenes,\n");
+    printf("Where logic and beauty intertwine as it seems.\n");
+   
+    return 0;
+}
